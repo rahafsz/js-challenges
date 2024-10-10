d1f2a4c5c82db747b0c4055f8247a166c6239ac2
RahafZourob <zourobrahaf@gmail.com>
2024-10-10 20:55:02 +0300

    edit: avgDolphins variable name

--- challenge-5/script.js
+++ challenge-5/script.js
@@ -1,15 +1,15 @@
 calcAverage = (score1, score2, score3) => {
     return (score1 + score2 + score3) / 3;
 };
-const avgDolhins = calcAverage(85, 54, 41);
+const avgDolphins = calcAverage(85, 54, 41);
 const avgKoalas = calcAverage(23, 34, 27);
-checkWinner = (avgDolhins, avgKoalas) => {
-    if (avgDolhins > avgKoalas && avgDolhins >= avgKoalas * 2) {
-        console.log(`Dolhins win (${avgDolhins} vs. ${avgKoalas})`);
-    } else if (avgKoalas > avgDolhins && avgKoalas >= avgDolhins * 2) {
-        console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`);
+checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgDolphins > avgKoalas && avgDolphins >= avgKoalas * 2) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas > avgDolphins && avgKoalas >= avgDolphins * 2) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
     } else {
         console.log("No one wins");
     }
 };
-checkWinner(avgDolhins, avgKoalas);
+checkWinner(avgDolphins, avgKoalas);

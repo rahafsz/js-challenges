dc769c5ec8bca6049d66201aa0d2241b89dc85a4
RahafZourob <zourobrahaf@gmail.com>
2024-10-10 21:05:26 +0300

    edit: control statement condition

--- challenge-5/script.js
+++ challenge-5/script.js
@@ -2,12 +2,12 @@ calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 const avgDolphins = calcAverage(85, 54, 41);
 const avgKoalas = calcAverage(23, 34, 27);
 checkWinner = (avgDolphins, avgKoalas) => {
-    if (avgDolphins > avgKoalas && avgDolphins >= avgKoalas * 2) {
+    if (avgDolphins >= avgKoalas * 2) {
         console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
-    } else if (avgKoalas > avgDolphins && avgKoalas >= avgDolphins * 2) {
+    } else if (avgKoalas >= avgDolphins * 2) {
         console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
     } else {
-        console.log("No one wins");
+        console.log("No team wins");
     }
 };
 checkWinner(avgDolphins, avgKoalas);
